ae57609f2a9ffa4cc7a63611f6b32017893f7958
sandromatter <github@sandromatter.ch>
2020-05-08 19:35:54 +0200

    Tidy up links, new carousel, image container when image loading.

--- docs/js/script.js
+++ docs/js/script.js
@@ -23,3 +23,43 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
         }
     }
 });
+
+$(document).ready(function () {
+    $('.autoplay').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
+
